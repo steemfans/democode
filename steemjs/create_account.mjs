28642d06f3e6ccf2aec99553be16d3f9eb3b5227
ety001 <work@akawa.ink>
2024-04-28 02:09:35 +0800

    update

--- steemjs/create_account.mjs
+++ steemjs/create_account.mjs
@@ -1,6 +1,6 @@
 import steem from 'steem';
 
-steem.api.setOptions({ url: 'http://172.20.0.2:8090'});
+steem.api.setOptions({ url: 'http://test1.steem.fans:8090'});
 steem.config.set('address_prefix','TST');
 steem.config.set('chain_id','18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e');
 
