6df9394394bbf75f6d357637461d8fc899cee959
ety001 <work@akawa.ink>
2024-04-28 02:05:44 +0800

    update

--- steemjs-create-account/index.mjs
+++ steemjs-create-account/index.mjs
@@ -11,73 +11,86 @@ const new_account_name = 'ety001.test';
 const new_account_priv = '';
 const new_account_pub = 'TST8Vf3uwM1to43pe1omHbuLkouW8zMSooeaBSDZCbGHnf2Y4kcbS';
 
-const claim_op = [
-  "claim_account",
-  {
-    "fee": "0.000 TESTS",
-    "creator": account_name,
-    "extensions": []
-  }
-];
 
-const create_account_op = [
-  "create_claimed_account",
-  {
-    "creator": account_name,
-    "new_account_name": new_account_name,
-    "owner": {
-      "weight_threshold": 1,
-      "account_auths": [],
-      "key_auths": [
-        [
-          new_account_pub,
-          1
-        ]
-      ]
-    },
-    "active": {
-      "weight_threshold": 1,
-      "account_auths": [],
-      "key_auths": [
-        [
-          new_account_pub,
-          1
-        ]
-      ]
-    },
-    "posting": {
-      "weight_threshold": 1,
-      "account_auths": [],
-      "key_auths": [
-        [
-          new_account_pub,
-          1
-        ]
-      ]
-    },
-    "memo_key": new_account_pub,
-    "json_metadata": "{}"
-  }
-]
+async function create_account(account_name, new_account_name, new_account_pub, active_key) {
+  const claim_op = [
+    "claim_account",
+    {
+      "fee": "0.000 TESTS",
+      "creator": account_name,
+      "extensions": []
+    }
+  ];
 
-steem.broadcast.send({
-  extensions: [],
-  operations: [
-    claim_op,
-  ]
-}, [active_key], (err, result) => {
-  console.log(err, result);
-  if (err === null) {
-    steem.broadcast.send({
-      extensions: [],
-      operations: [
-        create_account_op,
-      ]
-    }, [active_key], (err2, res2) => {
-      console.log(err2, res2);
+  const create_account_op = [
+    "create_claimed_account",
+    {
+      "creator": account_name,
+      "new_account_name": new_account_name,
+      "owner": {
+        "weight_threshold": 1,
+        "account_auths": [],
+        "key_auths": [
+          [
+            new_account_pub,
+            1
+          ]
+        ]
+      },
+      "active": {
+        "weight_threshold": 1,
+        "account_auths": [],
+        "key_auths": [
+          [
+            new_account_pub,
+            1
+          ]
+        ]
+      },
+      "posting": {
+        "weight_threshold": 1,
+        "account_auths": [],
+        "key_auths": [
+          [
+            new_account_pub,
+            1
+          ]
+        ]
+      },
+      "memo_key": new_account_pub,
+      "json_metadata": "{}"
+    }
+  ];
+
+  const sendPromise = function(op, active_key) {
+    return new Promise((resolve, reject) => {
+      steem.broadcast.send({
+        extensions: [],
+        operations: [
+          op,
+        ]
+      }, [active_key], (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
     });
   }
-});
+
+  let err, res = await sendPromise(claim_op, active_key);
+  err, res = await sendPromise(create_account_op, active_key);
+  console.log(res);
+}
+
+async function main() {
+  for(let i = 1; i <= 30; i++) {
+    await create_account(account_name, 'test-account-' + i, new_account_pub, active_key);
+  }
+}
+
+main();
 
 // steem.api.call('database_api.get_dynamic_global_properties', {}, function(err, result) {
 //   console.log(err, result);
