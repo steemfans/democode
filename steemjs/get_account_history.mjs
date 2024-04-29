f2cc4b941d89a6f122b8ad1be67d051184a80b5e
ety001 <work@akawa.ink>
2024-04-29 16:19:35 +0800

    update

--- steemjs/get_account_history.mjs
+++ steemjs/get_account_history.mjs
@@ -18,7 +18,8 @@ async function desc() {
   let from = -1, page = 5;
   let result, tmp;
 
-  for (let p = 1; p <= 5; p++) {
+  for (let p = 1; p <= page; p++) {
+    console.log('from => ', from, 'limit => ', limit)
     result = await getAccountHistory(username, from, limit);
     tmp = result.reverse()
     for(let i = 0; i < limit; i++) {
@@ -27,11 +28,40 @@ async function desc() {
       console.log("trx_in_block:", tmp[i][1]['trx_in_block']);
       console.log("timestamp:", tmp[i][1]['timestamp']);
       console.log("op:", tmp[i][1]['op']);
-      console.log("---------"); 
+      console.log("------------------"); 
     }
     console.log(`--------- page${p} ---------`); 
     from = tmp[limit][0];
   }
 }
 
-desc();
+async function asc() {
+  const username = 'ety001';
+  const limit = 3;
+  let from = limit, page = 5;
+  let result, tmp;
+
+  for (let p = 1; p <= page; p++) {
+    console.log('from => ', from, 'limit => ', limit)
+    result = await getAccountHistory(username, from, limit);
+    for(let i = 0; i < limit; i++) {
+      console.log("id:", result[i][0]);
+      console.log("block_num:", result[i][1]['block']);
+      console.log("trx_in_block:", result[i][1]['trx_in_block']);
+      console.log("timestamp:", result[i][1]['timestamp']);
+      console.log("op:", result[i][1]['op']);
+      console.log("------------------"); 
+    }
+    console.log(`--------- page${p} ---------`); 
+    from = result[limit][0] + limit;
+  }
+}
+
+async function main() {
+  console.log("======> desc() <======");
+  await desc();
+  console.log("======> asc() <======");
+  await asc();
+}
+
+main();
